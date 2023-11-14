9c60fde52922ff0f62b825b832bb4e7cb252a688
Varun Nair <varunsnair1997@gmail.com>
2023-11-14 07:36:57 +0530

    qrcode

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,26 @@
-import { Box, Button, ButtonGroup, Center, Flex, Grid, Link, grid } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup, Center, Flex, Grid, Img, Link, Text, grid } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { deletePayment, getData, patchData, postPayment } from '../Redux/Data/action'
 import { color } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
-
+import QRCode from 'qrcode'
+import uuid4 from "uuid4";
 function Home() {
 
   const mainData = useSelector((store)=>store.data.data)
-  let userData=localStorage.getItem("token")
+  let userData = localStorage.getItem("token")
   const dispatch=useDispatch()
   const [select,setSelect]=useState([])
   const navigate=useNavigate()
   const [seat,setSeat]=useState([])
+  const [qrCodeImage,setQrCodeImage]=useState("")
+  const [payment,setPayment]=useState([])
   
+  var paymentId = uuid4();
+
+
+
 
   console.log("maindata",mainData)
 
@@ -36,6 +43,30 @@ function Home() {
 
   const handlePayment = async() => {
   
+    let qr = seat.map((item)=>{
+      return item.seat
+    })
+       
+    
+   
+let qrData = qr.join("-")
+  console.log("qrData",qrData)
+  
+    QRCode.toDataURL(qrData)
+    .then(url => {
+      setQrCodeImage(url)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+    
+   seat.map((item)=>(
+    dispatch(postPayment(item))
+   ))
+ 
+
+
+
 
     try {
       await Promise.all(seat.map((element) => dispatch(patchData(element.booked, element.id))));  
@@ -44,30 +75,56 @@ function Home() {
       console.error('Error updating data:', error);
     }
   };
+  
+
 
 
   const handleChoose = (data,index,userId,id)=>{
     let editedData = [...select]
 
     console.log("userData",userData,userId)
+    
     if(userId===userData){
       editedData[index].booked === true?editedData[index].booked = false:editedData[index].booked = true
      let data = {
       booked:editedData[index].booked,
-      id:editedData[index].id
+      id:editedData[index].id,
+      price:editedData[index].price,
+      seat:editedData[index].seat
      }
      setSeat([...seat,data])
+
     }
     
     setSelect(editedData)
   
   }
-  
+
+
+  let total = seat?.reduce((acc,item)=>{
+    return  item.price + acc
+  },0)
+
+  console.log("total",total)
+
+
+
+// useEffect(()=>{
+//   let paymentData = {
+//     seatNo:seat.map((item)=>{return item.seat}),
+//     paymentId:paymentId,
+//     total:seat.reduce((acc,item)=>{return acc+item.price},0),
+//     numberOfSeat:seat.length,
+//     url:qrCodeImage
+//   }
+//   dispatch(postPayment(paymentData))
+// },[qrCodeImage])
+
 
 
   return (
 
-    <Grid> Select your seat
+    <Grid  > Select your seat
       <Link marginRight={1400} onClick={handleBack}>Back</Link>
       
       <Link marginRight={1400} onClick={handleNext}>Next</Link>
@@ -88,12 +145,17 @@ function Home() {
       ))}
 
 
-      
-
+     
+   
+     
+          <Text> Total: {total} Rs</Text>
+     
 
     </Grid>
     <Button marginTop={10} marginLeft={550} width={200} onClick={handlePayment}>Payment</Button>
-    </Grid>
+    <Img  src={qrCodeImage} />
+    </Grid >
+    
 
   )
 }
--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { deletePayment, getPayment, postPayment } from '../Redux/Data/action'
-import { Button, Grid, Link } from '@chakra-ui/react'
+import { Button, Grid, Img, Link } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
+import  QRCode  from 'qrcode'
+import uuid4 from 'uuid4'
 
 function Payment() {
 
     const payment=useSelector((store)=>store.data.getPayment)
     const dispatch=useDispatch()
     const navigate=useNavigate()
+    const [qrCodeImage,setQrCodeImage]=useState("")
+
+    let paymentId = uuid4();
 
     console.log("payment",payment)
 
@@ -19,19 +24,61 @@ function Payment() {
     const handleBack=()=>{
         navigate(-1)
     }
-    
 
+    let seatQr={
+        seat:payment.map((item)=>{return item.seat}),
+        paymentId:paymentId,
+        price:payment.map((item)=>{return item.price}),
+        numberOfSeats:payment.length
+    }
+
+    let qrData = seatQr.join("-")
+  console.log("qrData",qrData)
+  
+    QRCode.toDataURL(qrData)
+    .then(url => {
+      setQrCodeImage(url)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+    
+console.log("xxxxxxxxx",qrCodeImage)
   return (
     <div>Payment
         <Grid><Link marginRight={1350} onClick={handleBack}>Back</Link></Grid>
 
-        {payment.map((item)=>(
+        {/* {payment.map((item)=>(
             <>
             {item.seat}
             </>
-        ))}
+        ))} */}
+
+<Img  src={qrCodeImage} />
+ 
     </div>
+
+
   )
 }
 
 export default Payment
+
+
+
+
+
+
+// 
+// const handleChoose=()=>{
+// let editeddata=[...select]
+// if(editedData.userId===userData){
+//   editedData.booked===true?editedData.booked=false:editedData.booked=true
+// }
+// let data={
+//    booked=editedData.booked
+//    id=editedData.id
+//    seat=editedData.seat
+// }
+// setSeat([...seat])
+// }
